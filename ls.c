a268d60d3db9debc0585a6709b5954f8850ebe32
aguiarcode <252622789+aguiarcode@users.noreply.github.com>
2026-06-30 12:25:31 +0100

    argv and argc understanding

--- ls.c
+++ ls.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) { 
-    printf("This is my learning journey in C. \n"); 
+int main (int argc, char *argv[]) { 
+    printf("%s \n", argv[0]); 
     return 0; 
 }
