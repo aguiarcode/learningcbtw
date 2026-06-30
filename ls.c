3318112d2351ae00105b6956c0cb72ab5d9de50c
aguiarcode <252622789+aguiarcode@users.noreply.github.com>
2026-06-30 12:42:32 +0100

    treating errors with perror func

--- ls.c
+++ ls.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <errno.h>
 int main (int argc, char *argv[]) { 
     const char *path = (argc > 1) ? argv[1] : ".";
     
     DIR *dir = opendir(path);
+    if(!dir) {
+     perror("opendir");
+     return 1;   
+    }
 
     return 0; 
 }
